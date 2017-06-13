1e709afac626c0308dba4ccb8b7a7f1593fa7832
Visnja Krsmanovic <visnja.krsmanovic@zuehlke.com>
2017-06-13 11:24:03 +0200

    Coding style fixed

--- hello.c
+++ hello.c
@@ -4,15 +4,14 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Visnja Krsmanovic");
 
-__init static int hello_init(void)
+static int __init hello_init(void)
 {
-  pr_info("Hello world!\n");
-  return 0;
+	pr_info("Hello world!\n");
+	return 0;
 }
-__exit static void hello_exit(void)
+static void __exit hello_exit(void)
 {
-  pr_info("See ya world!\n");
-  return;
+	pr_info("See ya world!\n");
 }
 module_init(hello_init);
 module_exit(hello_exit);
