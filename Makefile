792cbf1abd8e7b018e5d4ae55146eecf111822d0
Visnja Krsmanovic <visnja.krsmanovic@zuehlke.com>
2017-06-13 10:02:30 +0200

    Hello world.

---
+++ Makefile
@@ -0,0 +1,9 @@
+obj-m += hello.o
+
+CFLAGS_hello.o += -DEBUG
+
+all:
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+
+clean:
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
---
+++ hello.c
@@ -0,0 +1,18 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Visnja Krsmanovic");
+
+__init static int hello_init(void)
+{
+  pr_info("Hello world!\n");
+  return 0;
+}
+__exit static void hello_exit(void)
+{
+  pr_info("Ciao world!\n");
+  return;
+}
+module_init(hello_init);
+module_exit(hello_exit);
